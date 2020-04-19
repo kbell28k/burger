7b2030ca64f6b9ea7c3f6a67afcbd495ed4cbc07
Mona Lisa <kbell28k@gmail.com>
2020-04-19 16:26:31 -0600

    setup of directory files

---
+++ db/schema.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers
+(
+    id int NOT NULL
+    AUTO_INCREMENT,
+  burger_name varchar
+    (255) not null,
+  devoured boolean default false,
+  primary key
+    (id)
+);
+
---
+++ db/seeds.sql
@@ -0,0 +1,20 @@
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('classic', true);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('double-zesty', true);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('classic', false);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('yum-yum-supreme', true);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('double-zesty', true);
